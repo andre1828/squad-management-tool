13bb955e8663c439dae4e44bb9135cb80fcc2c61
André Rodrigues <=>
2021-03-27 22:09:59 -0300

    implement player seach placeholder

--- src/ConfigureSquad/ConfigureSquad.js
+++ src/ConfigureSquad/ConfigureSquad.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './ConfigureSquad.css'
 import TeamFormation from './../TeamFormation/TeamFormation.js'
+import SearchPlayers from './../SearchPlayers/SearchPlayers.js'
 
 function ConfigureSquad() {
     return (
         <>
             <h3 className="section-title">Configure Squad</h3>
             <TeamFormation/>
+            <SearchPlayers/>
         </>
     )
 }
---
+++ src/SearchPlayers/SearchPlayers.js
@@ -0,0 +1,29 @@
+import './SearchPlayers.css'
+import React from 'react'
+
+function SearchPlayers() {
+    return (
+        <>
+
+            <div className="form-input">
+                <label htmlFor="searchPlayers">Search players</label>
+                <input type="text" id="searchPlayers" />
+            </div>
+            <ul className="player-list">
+                <li className="player">
+                    <p>Name:
+                        <span className="value">Cristiano Ronaldo</span>
+                    </p>
+                    <p>Age:
+                        <span className="value">32</span>
+                    </p>
+                    <p>Nationality:
+                        <span className="value">Portugal</span>
+                    </p>
+                </li>
+            </ul>
+        </>
+    )
+}
+
+export default SearchPlayers
