54d846085eb772e77efa6c5a953f800194c9f943
André Rodrigues <=>
2021-03-29 00:09:59 -0300

    footer layout adjustments

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -5,7 +5,6 @@ function Footer() {
     return (
         <footer>
             <h5>2010 All rights reserved</h5>
-            <h5>https://stackoverflow.com/questions/643879/css-to-make-html-page-footer-stay-at-bottom-of-the-page-with-a-minimum-height-b</h5>
         </footer>
     )
 }
